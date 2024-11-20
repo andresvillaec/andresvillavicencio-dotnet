1d0336d9e4020781f2141d7bb23c0105c4b2cc1e
Andres Villavicencio <avillavicencio@devsu.com>
2024-11-20 06:38:28 -0500

    fix: Set right ports on docker

---
+++ AccountService.Api/Data/AccountServiceContext.cs
@@ -0,0 +1,26 @@
+﻿using AccountService.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountService.Api.Data
+{
+    public class AccountServiceContext : DbContext
+    {
+        public AccountServiceContext(DbContextOptions<AccountServiceContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Movement> Movements { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships
+            modelBuilder.Entity<Account>()
+                .HasMany(a => a.Movements)
+                .WithOne(m => m.Account)
+                .HasForeignKey(m => m.AccountId);
+
+            // Add configurations if needed
+        }
+    }
+}
